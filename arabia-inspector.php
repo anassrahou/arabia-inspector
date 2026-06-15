02a90e83f979b87f119b1b7f2ef83b75dc574c87
Anass Rahou <nsrahou@gmail.com>
2026-06-15 04:03:43 -0500

    Remove unused loader class

--- arabia-inspector.php
+++ arabia-inspector.php
@@ -14,10 +14,21 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'AI_VERSION', '1.0.0-alpha' );
-define( 'AI_PLUGIN_FILE', __FILE__ );
-define( 'AI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
-define( 'AI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+if ( ! defined( 'AI_VERSION' ) ) {
+    define( 'AI_VERSION', '1.0.0-alpha' );
+}
+
+if ( ! defined( 'AI_PLUGIN_FILE' ) ) {
+    define( 'AI_PLUGIN_FILE', __FILE__ );
+}
+
+if ( ! defined( 'AI_PLUGIN_DIR') ) {
+    define( 'AI_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+
+if ( ! defined( 'AI_PLUGIN_URL') ) {
+    define( 'AI_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
 
 require_once AI_PLUGIN_DIR . 'includes/Core/Plugin.php';
 
