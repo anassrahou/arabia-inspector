481aa7c43e5701917ab4fad32cb24e006cdc96c9
Anass Rahou <nsrahou@gmail.com>
2026-06-20 23:36:52 -0500

    Create report exporter class

---
+++ includes/Core/Report_Exporter.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace AI\Core;
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+class Report_Exporter {
+
+    public static function generate_report() {
+
+        return '';
+    }
+}
